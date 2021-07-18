c9b79c040f9c6860987426fdf6284900dc243946
Lrd134 <larryc3200@gmail.com>
2021-07-18 18:15:58 -0400

    commit tree 72d5ca12a073997b6e91b098aa8aa6cb0bb206a6

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-
+  accepts_nested_attributes_for :tags
   validates_presence_of :name, :content
+  def tags_attributes=(tags_attributes)
+    
+    tags_attributes.each do | attributes, values |
+      unless values == nil
+        unless values["name"] == ""
+          tag = Tag.find_or_create_by(values)
+          self.tags << tag
+        end
+      end
+    end
+
+  end
 end
